5993922a2d6f6c11b177f5409d2b436248132f7b
Alexander Mandrikov <mandrikov@pm.me>
2023-12-09 15:03:45 +0700

    update equals functions signatures

--- src/entities.ts
+++ src/entities.ts
@@ -29,7 +29,7 @@ export type MangaEntity = {
 
 export type EqualsFunc<T> = (e1: T | undefined, e2: T | undefined) => boolean;
 
-export const isAnimeEquals = (a1: AnimeEntity | undefined, a2: AnimeEntity | undefined) => {
+export const isAnimeEquals: EqualsFunc<AnimeEntity> = (a1, a2) => {
   if (!a1 || !a2) return false;
 
   return a1.status === a2.status &&
@@ -37,7 +37,7 @@ export const isAnimeEquals = (a1: AnimeEntity | undefined, a2: AnimeEntity | und
     a1.episodesWatched === a2.episodesWatched;
 };
 
-export const isMangaEquals = (a1: MangaEntity | undefined, a2: MangaEntity | undefined) => {
+export const isMangaEquals: EqualsFunc<MangaEntity> = (a1, a2) => {
   if (!a1 || !a2) return false;
 
   return a1.status === a2.status &&
